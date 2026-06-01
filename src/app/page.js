1ef663618b36d740790b8072aa1d5801244ac654
theoitssurabaya <theopinem05@gmail.com>
2026-06-01 21:29:03 +0700

    change nav bar to top

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Dock from "@/components/Dock";
+import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import Education from "@/components/Education";
@@ -10,12 +10,13 @@ import FloatingParticles from "@/components/FloatingParticles";
 export default function Home() {
   return (
     <>
+      <Navbar />
+
       {/* Overlay global: partikel melayang (di belakang konten) */}
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: 0, pointerEvents: 'none' }}>
         <FloatingParticles />
       </div>
 
-      <Dock />
       <Hero />
       <Skills />
       <Education />
