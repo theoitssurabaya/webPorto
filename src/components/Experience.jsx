9671ab22d7ce10f3db244569bdf33b9c02fc8006
Theo <theopinem05@gmail.com>
2026-07-11 08:41:13 +0700

    feat: scrollable experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -59,7 +59,7 @@ export default function Experience() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
                         </div>
                         <div className="selector-tab-indicator"></div>
-                        <div className={`selector-tabs ${isDropdownOpen ? 'open' : ''}`}>
+                        <div className={`selector-tabs ${isDropdownOpen ? 'open' : ''}`} data-lenis-prevent="true">
                             {tabs.map(tab => (
                                 <button key={tab.id} className={`selector-tab ${activeTab === tab.id ? 'active' : ''}`} onClick={() => handleTabClick(tab.id)}>
                                     <span className="tab-title">{tab.title}</span>
